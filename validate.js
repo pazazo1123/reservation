fde1cfead4bd0b2b22c1b831fce8cd327697507f
fa-is <2410511101064@live4.utcc.ac.th>
2026-04-01 14:55:10 +0700

    Step 1: Add validation for all required fields

--- validate.js
+++ validate.js
@@ -17,6 +17,12 @@ function checkTicketNo() {
 }
 
 function validateForm(){
+	let fname = document.getElementById("fname").value.trim();
+	let lname = document.getElementById("lname").value.trim();
+	if (fname === "" || lname === "") { 
+    alert("Please fill in all fields"); 
+    return false; 
+}
 	if(!checkNID()){
 	  alert("Invalid value for National ID!");
 	  document.getElementById("nid").focus();
